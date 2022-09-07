aa0bf30277ea2bd64b13e237ab4e1bca8d3bbcc4
Rupali <gajerupali81@mail.com>
2022-09-08 00:07:23 +0530

    commit message

---
+++ src/main/java/com/validation/SpringBeanValidation1Application.java
@@ -0,0 +1,13 @@
+package com.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanValidation1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeanValidation1Application.class, args);
+	}
+
+}
